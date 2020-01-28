1d452f8115070570b69a2a19c95747ba26c3cf6e
Michael Stromer <stroms38@gmail.com>
2020-01-28 09:23:18 -0500

    gen hooks

--- graphql/graphql-hooks.ts
+++ graphql/graphql-hooks.ts
@@ -4,106 +4,103 @@ import * as ApolloReactHooks from '@apollo/react-hooks';
 export type Maybe<T> = T | null;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string;
-  String: string;
-  Boolean: boolean;
-  Int: number;
-  Float: number;
-  DateTime: any;
+  ID: string,
+  String: string,
+  Boolean: boolean,
+  Int: number,
+  Float: number,
+  DateTime: any,
 };
+
 
 export type Mutation = {
-  __typename?: 'Mutation';
-  createPlace: Place;
+   __typename?: 'Mutation',
+  createPlace: Place,
 };
 
+
 export type MutationCreatePlaceArgs = {
-  place: PlaceInput;
+  place: PlaceInput
 };
 
 export type Place = {
-  __typename?: 'Place';
-  id: Scalars['ID'];
-  title: Scalars['String'];
-  description?: Maybe<Scalars['String']>;
-  imageUrl?: Maybe<Scalars['String']>;
-  creationDate: Scalars['DateTime'];
+   __typename?: 'Place',
+  id: Scalars['ID'],
+  title: Scalars['String'],
+  description?: Maybe<Scalars['String']>,
+  imageUrl?: Maybe<Scalars['String']>,
+  creationDate: Scalars['DateTime'],
 };
 
 export type PlaceInput = {
-  title: Scalars['String'];
-  description?: Maybe<Scalars['String']>;
-  imageUrl?: Maybe<Scalars['String']>;
+  title: Scalars['String'],
+  description?: Maybe<Scalars['String']>,
+  imageUrl?: Maybe<Scalars['String']>,
 };
 
 export type Query = {
-  __typename?: 'Query';
-  place?: Maybe<Place>;
-  places: Array<Place>;
+   __typename?: 'Query',
+  place?: Maybe<Place>,
+  places: Array<Place>,
 };
 
+
 export type QueryPlaceArgs = {
-  id: Scalars['Float'];
+  id: Scalars['Float']
 };
 
 export type CreatePlaceMutationVariables = {
-  title: Scalars['String'];
-  description: Scalars['String'];
-  imageUrl: Scalars['String'];
+  title: Scalars['String'],
+  description: Scalars['String'],
+  imageUrl: Scalars['String']
 };
 
-export type CreatePlaceMutation = { __typename?: 'Mutation' } & {
-  createPlace: { __typename?: 'Place' } & Pick<
-    Place,
-    'id' | 'title' | 'description' | 'imageUrl' | 'creationDate'
-  >;
-};
+
+export type CreatePlaceMutation = (
+  { __typename?: 'Mutation' }
+  & { createPlace: (
+    { __typename?: 'Place' }
+    & Pick<Place, 'id' | 'title' | 'description' | 'imageUrl' | 'creationDate'>
+  ) }
+);
 
 export type PlaceQueryVariables = {
-  id: Scalars['Float'];
+  id: Scalars['Float']
 };
 
-export type PlaceQuery = { __typename?: 'Query' } & {
-  place: Maybe<
-    { __typename?: 'Place' } & Pick<
-      Place,
-      'id' | 'title' | 'description' | 'imageUrl' | 'creationDate'
-    >
-  >;
-};
+
+export type PlaceQuery = (
+  { __typename?: 'Query' }
+  & { place: Maybe<(
+    { __typename?: 'Place' }
+    & Pick<Place, 'id' | 'title' | 'description' | 'imageUrl' | 'creationDate'>
+  )> }
+);
 
 export type PlacesQueryVariables = {};
 
-export type PlacesQuery = { __typename?: 'Query' } & {
-  places: Array<
-    { __typename?: 'Place' } & Pick<
-      Place,
-      'id' | 'title' | 'description' | 'imageUrl' | 'creationDate'
-    >
-  >;
-};
+
+export type PlacesQuery = (
+  { __typename?: 'Query' }
+  & { places: Array<(
+    { __typename?: 'Place' }
+    & Pick<Place, 'id' | 'title' | 'description' | 'imageUrl' | 'creationDate'>
+  )> }
+);
+
 
 export const CreatePlaceDocument = gql`
-  mutation CreatePlace(
-    $title: String!
-    $description: String!
-    $imageUrl: String!
-  ) {
-    createPlace(
-      place: { title: $title, description: $description, imageUrl: $imageUrl }
-    ) {
-      id
-      title
-      description
-      imageUrl
-      creationDate
-    }
+    mutation CreatePlace($title: String!, $description: String!, $imageUrl: String!) {
+  createPlace(place: {title: $title, description: $description, imageUrl: $imageUrl}) {
+    id
+    title
+    description
+    imageUrl
+    creationDate
   }
-`;
-export type CreatePlaceMutationFn = ApolloReactCommon.MutationFunction<
-  CreatePlaceMutation,
-  CreatePlaceMutationVariables
->;
+}
+    `;
+export type CreatePlaceMutationFn = ApolloReactCommon.MutationFunction<CreatePlaceMutation, CreatePlaceMutationVariables>;
 
 /**
  * __useCreatePlaceMutation__
@@ -124,44 +121,29 @@ export type CreatePlaceMutationFn = ApolloReactCommon.MutationFunction<
  *   },
  * });
  */
-export function useCreatePlaceMutation(
-  baseOptions?: ApolloReactHooks.MutationHookOptions<
-    CreatePlaceMutation,
-    CreatePlaceMutationVariables
-  >
-) {
-  return ApolloReactHooks.useMutation<
-    CreatePlaceMutation,
-    CreatePlaceMutationVariables
-  >(CreatePlaceDocument, baseOptions);
-}
-export type CreatePlaceMutationHookResult = ReturnType<
-  typeof useCreatePlaceMutation
->;
-export type CreatePlaceMutationResult = ApolloReactCommon.MutationResult<
-  CreatePlaceMutation
->;
-export type CreatePlaceMutationOptions = ApolloReactCommon.BaseMutationOptions<
-  CreatePlaceMutation,
-  CreatePlaceMutationVariables
->;
+export function useCreatePlaceMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<CreatePlaceMutation, CreatePlaceMutationVariables>) {
+        return ApolloReactHooks.useMutation<CreatePlaceMutation, CreatePlaceMutationVariables>(CreatePlaceDocument, baseOptions);
+      }
+export type CreatePlaceMutationHookResult = ReturnType<typeof useCreatePlaceMutation>;
+export type CreatePlaceMutationResult = ApolloReactCommon.MutationResult<CreatePlaceMutation>;
+export type CreatePlaceMutationOptions = ApolloReactCommon.BaseMutationOptions<CreatePlaceMutation, CreatePlaceMutationVariables>;
 export const PlaceDocument = gql`
-  query Place($id: Float!) {
-    place(id: $id) {
-      id
-      title
-      description
-      imageUrl
-      creationDate
-    }
+    query Place($id: Float!) {
+  place(id: $id) {
+    id
+    title
+    description
+    imageUrl
+    creationDate
   }
-`;
+}
+    `;
 
 /**
  * __usePlaceQuery__
  *
  * To run a query within a React component, call `usePlaceQuery` and pass it any options that fit your needs.
- * When your component renders, `usePlaceQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * When your component renders, `usePlaceQuery` returns an object from Apollo Client that contains loading, error, and data properties 
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
@@ -173,51 +155,32 @@ export const PlaceDocument = gql`
  *   },
  * });
  */
-export function usePlaceQuery(
-  baseOptions?: ApolloReactHooks.QueryHookOptions<
-    PlaceQuery,
-    PlaceQueryVariables
-  >
-) {
-  return ApolloReactHooks.useQuery<PlaceQuery, PlaceQueryVariables>(
-    PlaceDocument,
-    baseOptions
-  );
-}
-export function usePlaceLazyQuery(
-  baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
-    PlaceQuery,
-    PlaceQueryVariables
-  >
-) {
-  return ApolloReactHooks.useLazyQuery<PlaceQuery, PlaceQueryVariables>(
-    PlaceDocument,
-    baseOptions
-  );
-}
+export function usePlaceQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<PlaceQuery, PlaceQueryVariables>) {
+        return ApolloReactHooks.useQuery<PlaceQuery, PlaceQueryVariables>(PlaceDocument, baseOptions);
+      }
+export function usePlaceLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<PlaceQuery, PlaceQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<PlaceQuery, PlaceQueryVariables>(PlaceDocument, baseOptions);
+        }
 export type PlaceQueryHookResult = ReturnType<typeof usePlaceQuery>;
 export type PlaceLazyQueryHookResult = ReturnType<typeof usePlaceLazyQuery>;
-export type PlaceQueryResult = ApolloReactCommon.QueryResult<
-  PlaceQuery,
-  PlaceQueryVariables
->;
+export type PlaceQueryResult = ApolloReactCommon.QueryResult<PlaceQuery, PlaceQueryVariables>;
 export const PlacesDocument = gql`
-  query Places {
-    places {
-      id
-      title
-      description
-      imageUrl
-      creationDate
-    }
+    query Places {
+  places {
+    id
+    title
+    description
+    imageUrl
+    creationDate
   }
-`;
+}
+    `;
 
 /**
  * __usePlacesQuery__
  *
  * To run a query within a React component, call `usePlacesQuery` and pass it any options that fit your needs.
- * When your component renders, `usePlacesQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * When your component renders, `usePlacesQuery` returns an object from Apollo Client that contains loading, error, and data properties 
  * you can use to render your UI.
  *
  * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
@@ -228,31 +191,12 @@ export const PlacesDocument = gql`
  *   },
  * });
  */
-export function usePlacesQuery(
-  baseOptions?: ApolloReactHooks.QueryHookOptions<
-    PlacesQuery,
-    PlacesQueryVariables
-  >
-) {
-  return ApolloReactHooks.useQuery<PlacesQuery, PlacesQueryVariables>(
-    PlacesDocument,
-    baseOptions
-  );
-}
-export function usePlacesLazyQuery(
-  baseOptions?: ApolloReactHooks.LazyQueryHookOptions<
-    PlacesQuery,
-    PlacesQueryVariables
-  >
-) {
-  return ApolloReactHooks.useLazyQuery<PlacesQuery, PlacesQueryVariables>(
-    PlacesDocument,
-    baseOptions
-  );
-}
+export function usePlacesQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<PlacesQuery, PlacesQueryVariables>) {
+        return ApolloReactHooks.useQuery<PlacesQuery, PlacesQueryVariables>(PlacesDocument, baseOptions);
+      }
+export function usePlacesLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<PlacesQuery, PlacesQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<PlacesQuery, PlacesQueryVariables>(PlacesDocument, baseOptions);
+        }
 export type PlacesQueryHookResult = ReturnType<typeof usePlacesQuery>;
 export type PlacesLazyQueryHookResult = ReturnType<typeof usePlacesLazyQuery>;
-export type PlacesQueryResult = ApolloReactCommon.QueryResult<
-  PlacesQuery,
-  PlacesQueryVariables
->;
+export type PlacesQueryResult = ApolloReactCommon.QueryResult<PlacesQuery, PlacesQueryVariables>;
